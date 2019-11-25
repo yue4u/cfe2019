95b8f34105dfb52973a0da178052f7e848be10a8
rainy-me <github@rainy.me>
2019-11-25 11:30:52 +0900

    add chapter 1~8

---
+++ pages/chapter-7.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="chapter">
+    <h1>chapter 7</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+.chapter {
+  max-width: 640px;
+  margin: 0 auto;
+  display: block;
+}
+</style>

eecb1e9afeec81bfb2b5b2b4582b5b7f0400a732
rainy-me <github@rainy.me>
2019-11-26 22:25:14 +0900

    add 6-3 animation

--- link-svg.py
+++ link-svg.py
@@ -5,10 +5,14 @@ xlink_re = re.compile('image id="(.+?)"[\s\S]+?(xlink:href=".+?)"')
 file_name = sys.argv[1]
 print(file_name)
 
+_ids = []
+
 
 def replace_link(match):
     _txt = match.group(0)
     _id = match.group(1)
+    global _ids
+    _ids.append(_id)
     _link = match.group(2)
     return _txt.replace(_link, f":xlink:href=\"require('@/assets/chapter-6/{_id}.png')")
 
@@ -19,9 +23,43 @@ with open(file_name, 'r') as f:
     print(o)
 
 raw_name = file_name.split('/')[-1].replace('assets', '').replace('.', '-')
-out_file_name = f"components/{raw_name}-tmp.vue"
+out_file_name = f"components/chapter-{raw_name}.vue"
 print(out_file_name)
-with open(f"components/{raw_name}-tmp.vue", 'w') as f:
-    f.write(f"""<template>
-{o}
-</template>""")
+with open(out_file_name, 'w') as f:
+    f.write("""<template>
+""" + o+"""
+</template>
+
+
+
+<script>
+import { gsap, Back, Linear } from 'gsap'
+import { fadeIn, appear, moving } from '@/helpers'
+
+export default {
+  mounted() {
+    setTimeout(() => {
+     gsap
+      .timeline({ delay: 0 })
+      .fromTo('#s2-head', appear.time, fadeIn.from, fadeIn.to)
+      .fromTo('#s2-body', appear.time, fadeIn.from, fadeIn.to)
+      .fromTo('#s2-pearl', appear.time, fadeIn.from, fadeIn.to)
+
+      .to('#s2-surprise', 0.2, {
+        opacity: 1,
+        rotate: 10,
+        transformOrigin: 'center center'
+      })
+    }, 1500)
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+""" + ",".join([f'#{__id}'for __id in _ids]) + """ {
+  opacity: 0;
+}
+</style>
+
+""")
--- pages/chapter-6-3.vue
+++ pages/chapter-6-3.vue
@@ -1,32 +1,101 @@
 <template>
   <main>
-    <chapter-6-stage-2 class="cfe-svg" />
-    <cfe-next v-if="next" to="/chapter-6-3" />
+    <svg
+      class="cfe-svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="1242"
+      height="2208"
+      viewBox="0 0 1242 2208"
+    >
+      <g id="stage-3">
+        <g id="s3-crystal">
+          <image
+            id="s3-p3"
+            x="141"
+            y="1481"
+            width="964"
+            height="590"
+            :xlink:href="require('@/assets/chapter-6/s3-p3.png')"
+          />
+          <image
+            id="s3-wind-2"
+            x="422"
+            y="1585"
+            width="474"
+            height="478"
+            :xlink:href="require('@/assets/chapter-6/s3-wind-2.png')"
+          />
+        </g>
+        <g id="s3-wind">
+          <image
+            id="s3-p2"
+            x="141"
+            y="823"
+            width="964"
+            height="596"
+            :xlink:href="require('@/assets/chapter-6/s3-p-2.png')"
+          />
+        </g>
+        <g id="s3-p1" data-name="s3-p1">
+          <image
+            id="s3-pearl"
+            x="141"
+            y="-269"
+            width="1100"
+            height="1298"
+            :xlink:href="require('@/assets/chapter-6/s3-p1.png')"
+          />
+          <image
+            id="s3-wind-2-2"
+            data-name="s3-wind"
+            x="333"
+            y="296"
+            width="302"
+            height="249"
+            :xlink:href="require('@/assets/chapter-6/s3-wind.png')"
+          />
+        </g>
+      </g>
+    </svg>
   </main>
 </template>
 
-<script>
-import Chapter6Stage2 from '@/components/chapter-6-stage-2-svg'
 
-import CfeNext from '@/components/next'
+
+<script>
+import { gsap, Back, Linear } from 'gsap'
+import { fadeIn, appear, moving } from '@/helpers'
+
 export default {
-  components: {
-    CfeNext,
-    Chapter6Stage2
-  },
-  data() {
-    return {
-      next: false
-    }
-  },
   mounted() {
     setTimeout(() => {
-      this.next = true
-    }, 4000)
+      gsap
+        .timeline({ delay: 0 })
+        .fromTo('#s3-p1', appear.time, fadeIn.from, fadeIn.to)
+        .fromTo('#s3-wind-2-2', appear.time, fadeIn.from, {
+          ...fadeIn.to,
+          x: -10
+        })
+        .fromTo('#s3-p2', appear.time, fadeIn.from, fadeIn.to)
+        .fromTo('#s3-p3', appear.time, fadeIn.from, fadeIn.to)
+        .fromTo('#s3-wind-2', appear.time, fadeIn.from, {
+          ...fadeIn.to,
+          x: -20
+        })
+    }, 1500)
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '@/assets/scss/main.scss';
+#s3-p3,
+#s3-p2,
+#s3-p1,
+#s3-wind-2,
+#s3-wind-2-2 {
+  opacity: 0;
+}
 </style>
+

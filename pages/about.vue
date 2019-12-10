6010919798143e34e3ea36d2e42b3b8189eabb03
rainy-me <github@rainy.me>
2019-12-10 16:15:06 +0900

    update about

--- pages/about.vue
+++ pages/about.vue
@@ -5,24 +5,14 @@
       <span class="cfe-green">旅</span>
       <span class="small">とは</span>
     </h1>
-
     <article class="about-txt">
+      <p>これは木人のケサリが破壊的な力を持つ緑の結晶を安全な場所に閉じ込むストーリーである。</p>
       <p>
-        この物語は木人のケサリが破壊的な力を持つ緑の結晶を安全な場所に閉じ込むストーリーである。
+        ストーリー中の緑の結晶は原子力発電に利用された核燃料の廃棄物である<span class="cfe-orange">高レベル放射性廃棄物</span>を表している。
       </p>
+      <p>ケサリの旅で出現した宇宙、海の底、そして地下は関連の研究機関が今までの研究で想定していた、その高レベル放射性廃棄物を閉じ込める場所だ。</p>
       <p>
-        その緑の結晶は原子力発電に利用された核燃料の廃棄物である<span
-          class="cfe-orange"
-          >高レベル放射線廃棄物</span
-        >の代物である。
-      </p>
-      <p>
-        ケサリの旅で出現した海の底、宇宙、また地下は関連の研究機関が今までの研究で想定していた、その高レベル放射線廃棄物を閉じ込む場所である。
-      </p>
-      <p>
-        <span class="cfe-orange"
-          >高レベル放射線の安全はケサリだけではなく、みんなにとって大事な問題であり、</span
-        >より詳しいコンテンツは以下のアイコンをクリック！
+        <span class="cfe-orange">その高レベル放射性廃棄物を閉じ込める場所だ。 高レベル放射性廃棄物の安全はケサリだけではなく、みんなにとって大事な問題である。</span>より詳しいコンテンツは以下のアイコンをクリック！
       </p>
       <p>
         <a
@@ -31,11 +21,8 @@
           rel="noopener noreferrer"
           href="https://www.numo.or.jp/pr-info/pr/video/"
         >
-          <img
-            class="numo-icon"
-            :src="require('@/assets/numo-icon.png')"
-            alt="numo"
-        /></a>
+          <img class="numo-icon" :src="require('@/assets/numo-icon.png')" alt="numo" />
+        </a>
       </p>
     </article>
   </main>
@@ -49,6 +36,7 @@
     color: var(--txt-green);
     padding: 4rem;
     font-size: 2rem;
+    line-height: 1.2;
     p {
       margin-bottom: 2rem;
     }
